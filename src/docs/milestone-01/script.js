0d668b038a1731bdb0ddd2fbc4f7dc87aa28ffbb
allysontrinh <allysontrinh@gmail.com>
2024-04-03 00:47:25 -0400

    Finalized bios, added comments section

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,22 +1,31 @@
-function ChangeCharacter(character)
-{
-    switch(character)
-    {
-        case "sarah":
-            document.getElementById("img").src = "images/sarah.png";
-            document.getElementById("name").innerHTML = "Sarah Jang";
-            break;
-        case "tanishka":
-            document.getElementById("img").src = "images/tanishka.png";
-            document.getElementById("name").innerHTML = "Tanishka Indorekar";
-            break;
-        case "allyson":
-            document.getElementById("img").src = "images/allyson.png";
-            document.getElementById("name").innerHTML = "Allyson Trinh";
-            break;
-        case "megan":
-            document.getElementById("img").src = "images/megan.png";
-            document.getElementById("name").innerHTML = "Megan Wong";
-            break;
+const field = document.querySelector('textarea');
+const backUp = field.getAttribute('placeholder')
+const submit = document.querySelector('#submit')
+// const comments = document.querySelector('#comment-box')
+const comments = document.getElementById('comment-box');
+
+
+// array to store the comments
+const comments_arr = [];
+
+// to generate html list based on comments array
+const display_comments = () => {
+  let commentsHTML = '';
+   comments_arr.forEach(comment => {
+    commentsHTML += `<p>${comment}</p>`;
+  })
+  comments.innerHTML = commentsHTML;
+}
+
+submit.onclick = function(event){
+    event.preventDefault();
+    const content = field.value;
+    if(content.length > 0){ // if there is content
+      // add the comment to the array
+      comments_arr.push(content);
+      // re-genrate the comment html list
+      display_comments();
+      // reset the textArea content 
+      field.value = '';
     }
 }
