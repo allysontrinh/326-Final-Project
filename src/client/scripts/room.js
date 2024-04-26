28c2e83e16ad016cf75dacb6e5e44fcd920b1bbd
Sarah Jang <ssjang@umass.edu>
2024-04-26 18:34:01 -0400

    Made textareas into grid layout for Notebook page. Added buttons for Room page

---
+++ src/client/scripts/room.js
@@ -0,0 +1,11 @@
+document.getElementById('notebook').addEventListener('click', function() {
+    window.location.href = '/src/client/notebook.html'; 
+});
+
+document.getElementById('wardrobe').addEventListener('click', function() {
+    window.location.href = '/src/client/wardrobe.html'; 
+});
+
+document.getElementById('logout').addEventListener('click', function() {
+    window.location.href = '/src/client/index/index.html'; 
+});
