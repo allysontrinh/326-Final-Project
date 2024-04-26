3950239d665d3dab396d0a1c27abe1b89fceba20
Sarah Jang <ssjang@umass.edu>
2024-04-26 11:40:13 -0400

    Updated notebook functionality to support content reload upon page reload and saving current content in the database

--- src/client/scripts/notebook.js
+++ src/client/scripts/notebook.js
@@ -1,4 +1,4 @@
-let db = null;
+let db = new PouchDB('notebook');
 const create = document.getElementById('create');
 const destroy = document.getElementById('destroy');
 const dump = document.getElementById('dump');
@@ -7,70 +7,158 @@ const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const entryName = document.getElementById('entry-name');
 const dropdown = document.getElementById('dropdown')
+let selectedOption = null
 
-window.addEventListener('load', async (event) => {
-    // Create a new PouchDB instance
-    db = new PouchDB('notebook');
-  
-    try {
-      // Get all documents in the database
-      const result = await db.allDocs({
-        include_docs: true,
-        attachments: true,
-      });
-      console.log(result)
-  
-    //   // Display the documents in the output element
-    //   result.rows.forEach((row) => {
-    //     const id = row.doc._id;
-    //     const value = row.doc.value;
-    //     page1.innerHTML = page1.innerHTML + '<br>' + id + ' : ' + value;
-    //   });
-    } catch (err) {
-      console.log(err);
+/**
+ * Prints all documents in the database. Used for debugging purposes.
+ */
+async function print(){
+  const result = await db.allDocs({
+    include_docs: true,
+    attachments: true,
+  });
+  result.rows.forEach((row) => {
+    console.log(row.doc._id)
+  });
+}
+
+/**
+ * Retrieves all documents from the database. 
+ * @returns {Array} - All documents from db
+ */
+function loadAllDocuments() {
+  return db.allDocs({include_docs: true })
+    .then(function (result) {
+      return result.rows.map(function (row) {
+      return row.doc;
+    });
+    })
+}
+
+/**
+ * Populates the dropdown menu with the options from the database.
+ * @param {Array} documents - Saved documents from the database 
+ */
+function populateDropdownOptions(documents) {
+  documents.forEach(function (doc) {
+    // Add options for each document except for currPage, which stores the
+    // current page that the user was on previously before reloading
+    if (doc._id !== "currPage"){
+      createDropdownOption(doc._id)
     }
   });
+}
 
-  destroy.addEventListener('click', async (event) => {
-    // Destroy the database
-    const res = await db.destroy();
-    page1.innerHTML = ''
-    page2.innerHTML = ''
+/**
+ * Creates a single dropdown menu option given a desired name for it. 
+ * @param {string} name 
+ */
+function createDropdownOption(name){
+  const newOption = document.createElement('option');
+  newOption.value = name;
+  newOption.textContent = name;
+  dropdown.appendChild(newOption);
+}
+
+/**
+ * Adds an event listener to the entire window. If reloaded, all documents will
+ * be loaded. Then, the previous dropdown menu will be repopulated with the options
+ * saved from the database. It sets the textareas to the previous values that the user was looking at
+ * before the window reloaded. 
+ */
+window.addEventListener('load', async (event) => {
+  loadAllDocuments()
+  .then(function (documents){
+    populateDropdownOptions(documents);
+    //restores page with previous values that user was looking at
+    db.get("currPage").then(function (doc){
+      page1.value = doc.page1 || ''
+      page2.value = doc.page2 || ''
+      // Still need to figure out how to make it load the option it selected before 
+      // page reload
+      // entryName.value = ''
+      // console.log(doc.entryName)
+      // let newOpt = dropdown.querySelector("option[value='" + doc.entryName + "']")
+      // newOpt.selected = true;
+    })
+  })
   });
 
+  /**
+   * Destroys the current selected option from the dropdown menu and database 
+   * when the delete button is clicked. It also resets the textareas. 
+   */
+  destroy.addEventListener('click', async (event) => {
+    let option = dropdown.options[dropdown.selectedIndex].textContent
+    if (option !== ''){
+      for (var i = 0; i < dropdown.options.length; i++) {
+        // Check if the textContent of the option matches the name to be removed
+        if (dropdown.options[i].textContent === option) {
+            // Remove the option
+            dropdown.remove(i);
+            break;
+        }
+      }
+      await db.get(option).then(function(doc) {
+        return db.remove(doc);})
+    }
+    page1.value = ''
+    page2.value = ''
+    entryName.value = ''
+  });
+
+  /**
+   * When "delete all entries" button is clicked, all documents are removed from the database
+   * and all dropdown options are deleted. All textareas are also reset. 
+   */
   dump.addEventListener('click', async (event) => {
-    //remove all documents 
+    //remove all documents from db
     db.allDocs().then(function (result) {
         return Promise.all(result.rows.map(function (row) {
           return db.remove(row.id, row.value.rev);
         }));
     })
 
-      //Remove dropdown options
-      while (dropdown.options.length > 0) {
-        dropdown.remove(0);
+    //Remove dropdown options
+    while (dropdown.options.length > 0) {
+      dropdown.remove(0);
     }
+    //Reset textareas 
+    page1.value = ''
+    page2.value = ''
+    entryName.value = ''
   })
   
-
-create.addEventListener('click', () =>{
-    page1.innerHTML = ''
-    page2.innerHTML = ''
-
+/**
+ * Provides a blank page for the user to type in. 
+ * Saves this page as the page the user last saw. 
+ */
+create.addEventListener('click', async () =>{
+    page1.value = ''
+    page2.value = ''
+    entryName.value = ''
+    await saveContentToDatabase()
 })
 
-
-dropdown.addEventListener('change', function() {
-    const selectedOption = dropdown.options[dropdown.selectedIndex].value;
-
+/**
+ * When a dropdown option is clicked, the text associated with that entry will be retrieved and
+ * displayed from the database. Saves this page as the page the user last saw. 
+ */
+dropdown.addEventListener('change', async function() {
+    selectedOption = dropdown.options[dropdown.selectedIndex].value;
+    //Displays saved text on notebook
     db.get(selectedOption).then(res => {
         page1.value = res.page1
         page2.value = res.page2
     })
-    console.log('Selected option:', selectedOption);
+    //Saves current text as currPage, so when user exits, they will see the last page they left off on
+    await saveContentToDatabase()
 });
 
-
+/**
+ * Saves the current textareas into the database with _id = entryName. Also creates a new dropdown menu
+ * option for this entry. Saves this page as the page the user last saw. Doesn't allow duplicate entryNames.
+ */
 save.addEventListener('click', async (event) => {
     try {
         //save notebook entries
@@ -83,13 +171,38 @@ save.addEventListener('click', async (event) => {
         });
 
         //Creates new dropdown option when "save-button" is clicked 
-        const dropdown = document.getElementById('dropdown');
-        const newOption = document.createElement('option');
-        newOption.value = entryName.value;
-        newOption.textContent = entryName.value;
-        dropdown.appendChild(newOption);
+        createDropdownOption(entryName.value)
+
+        //Change the current selected dropdown option to that^
+        let newOpt = dropdown.querySelector("option[value='" + entryName.value + "']")
+        newOpt.selected = true;
+
+        await saveContentToDatabase()
+
+        //Clear entryName
+        entryName.value = ''
     }
     catch (error){
         alert("Duplicate entry name. Please pick a different name.")
     }
 })
+
+/**
+ * Updates the currPage document in the database with the current textarea content. 
+ */
+async function saveContentToDatabase() {
+  await db.get("currPage")
+    .then(function(doc) {
+      doc.entryName = entryName.value
+      doc.page1 = page1.value
+      doc.page2 = page2.value 
+      return db.put(doc);
+    })
+}
+
+/**
+ * Whenever a user types in the textareas, we want to save the content into currPage in case
+ * they reload or navigate off the page. When they come back, this content will be reloaded. 
+ */
+page1.addEventListener("keyup", saveContentToDatabase)
+page2.addEventListener("keyup", saveContentToDatabase)
