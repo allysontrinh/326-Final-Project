76fcf9ecae7ee1bb8616d3ad7f92aec9cd6adf2c
Sarah Jang <ssjang@umass.edu>
2024-04-27 00:02:16 -0400

    Fisxed paths

--- src/client/scripts/index.js
+++ src/client/scripts/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         event.preventDefault(); // Prevent the default form submission 
 
         // Redirect to the "Room" page
-        window.location.href = '/src/client/room.html'; 
+        window.location.href = '../room.html'; 
     });
 });
 
