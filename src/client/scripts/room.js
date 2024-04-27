9f3cd02c02e75c3048be667d0e824bb65707217d
Sarah Jang <ssjang@umass.edu>
2024-04-26 23:31:50 -0400

    Tanishka: Implemented music player Megan: Implemented wardrobe styles+scripts+html

--- src/client/scripts/room.js
+++ src/client/scripts/room.js
@@ -9,3 +9,21 @@ document.getElementById('wardrobe').addEventListener('click', function() {
 document.getElementById('logout').addEventListener('click', function() {
     window.location.href = '/src/client/index.html'; 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let isPlaying = false;
+
+    document.getElementById('music-button').addEventListener('click', function() {
+        const audio = document.getElementById('study-music');
+        const musicButton = document.getElementById('music-button');
+        
+        if (isPlaying) {
+            audio.pause();
+            musicButton.innerHTML = "Play Music";
+        } else {
+            audio.play();
+            musicButton.innerHTML = "Stop Music";
+        }
+        isPlaying = !isPlaying;
+    });
+});
