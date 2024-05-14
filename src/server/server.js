1799d05f3bfd0615315132fe48869a72f49227b2
Sarah Jang <ssjang@umass.edu>
2024-05-13 23:37:40 -0400

    express server

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the client directory
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}/src/client/`);
+  });
