a8daae5a0480a52c739dfd85fb7747bbd09cb50c
Sarah Jang <ssjang@umass.edu>
2024-05-15 00:45:26 -0400

    foawe

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,7 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
 
-
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
