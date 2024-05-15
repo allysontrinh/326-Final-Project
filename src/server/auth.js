e33589265557c4b3e1b511c13c0650868002ac57
Sarah Jang <ssjang@umass.edu>
2024-05-14 22:51:55 -0400

    Auth

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,15 +1,3 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
 
-
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-// No default export is needed here if you are just setting up passport strategies
-export default passport;
