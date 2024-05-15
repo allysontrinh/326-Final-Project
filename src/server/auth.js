baf0de20c295c64c6803a4428ddc67be3da612b8
Sarah Jang <ssjang@umass.edu>
2024-05-14 22:51:08 -0400

    Auth

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,18 +1,7 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
 
-const GOOGLE_CLIENT_ID = 
-const GOOGLE_CLIENT_SECRET = 
 
-passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
-  passReqToCallback: true,
-},
-function(request, accessToken, refreshToken, profile, done) {
-  return done(null, profile);
-}));
 
 passport.serializeUser(function(user, done) {
   done(null, user);
